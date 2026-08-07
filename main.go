ae58f7261df6c7fdddb3f35c7f2c2f30b70a9ed6
suhag <suhagadhikari203@gmail.com>
2026-08-07 16:24:56 +0000

    7th aug  assignment , map

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import "fmt"
-
 func main() {
-	fmt.Println("Hello, World!")
-  	fmt.Println("hello golang class")
-	fmt.Println("sameline")
+user:=map[int]string{
+6:"Deepa",
+14:"Rajib",
+
+}
+user[6]="Mahim"
+_, okaygo:=user[6]
+fmt.Println(okaygo)
+fmt.Println(len(user))
+delete(user,6)
 }
